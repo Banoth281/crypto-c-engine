5d0d06bf711661832bb28ce8d447b3bede27fc5a
Santhosh Banoth <47032375+Banoth281@users.noreply.github.com>
2026-08-06 01:56:10 +0530

    Modify xor_encrypt_decrypt to use const char*
    Change input parameter type to const char* for better safety.

--- src/crypto.c
+++ src/crypto.c
@@ -1,7 +1,8 @@
-#include "crypto.h"
+#include <stdio.h>
 #include <string.h>
+#include "crypto.h"
 
-void xor_encrypt_decrypt(char *input, char key, char *output) {
+void xor_encrypt_decrypt(const char *input, char key, char *output) {
     size_t len = strlen(input);
     for (size_t i = 0; i < len; i++) {
         output[i] = input[i] ^ key;
